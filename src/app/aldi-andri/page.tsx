a466ee20b18f49d39b42d1e21e267390efd17321
febri ardiansyah <167492600+febriarr@users.noreply.github.com>
2026-03-06 13:54:21 +0000

    add og image

--- src/app/aldi-andri/page.tsx
+++ src/app/aldi-andri/page.tsx
@@ -5,6 +5,11 @@ import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "Wedding Invitation Aldi Pratama dan Andrianna Amaliyah Samsi",
   description: "Undangan Pernikahan Aldi Pratama dan Andrianna Amaliyah Samsi",
+  openGraph: {
+    title: "Wedding Invitation Aldi Pratama dan Andrianna Amaliyah Samsi",
+    description: "Undangan Pernikahan Aldi Pratama dan Andrianna Amaliyah Samsi",
+    images: [{ url: "https://invitation.solvrae.web.id/og-image.png" }]
+  }
 };
 export default function AldiAndriPage() {
   return (
